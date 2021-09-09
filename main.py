710d35a4a6c799a5948cd98e8a36bfac3c339a81
sushmitsb12 <90304129+sushmitsb12@users.noreply.github.com>
2021-09-09 16:05:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-class Song:
-    def __init__(self,lyrics):
-        self.lyrics = lyrics
-    def singasong(self):
-        for line in self.lyrics:
-            print(line)
-s = Song(['''May God Bless You,
-                    May God Bless You
-                    Happy Birthday to You'''])
-s.singasong()
+class Triangle:
+    def __init__(self,angle1,angle2,angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+
+    number_of_sides = 3
+    def check_Triangle(self):
+        if self.angle1 + self.angle2 + self.angle3 == 180:
+            return True
+        else:
+            return False
+class Equilateral(Triangle):
+    angle = 60
+    def __init__(self):
+        angle = 60
+        self.a1 = self.a2 = self.a3 = self.angle
+
+t = Triangle(60,90,30)
+print(t.check_Triangle())
